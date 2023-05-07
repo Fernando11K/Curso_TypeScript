6b74ed177a9f8fa1b24e2b8491c5e54736cdb049
Fernando11000 <fernandoferreira10@hotmail.com.br>
2023-05-06 22:43:35 -0300

    salva desenvolvimento

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,8 @@
 import { NegociacaoController } from "./controllers/negocicao-controller.js";
+
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', (event: Event) => {
     event.preventDefault();
     controller.adiciona();
-});
+})

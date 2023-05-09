dcf695247d64e4fbfbdbf72d2278ff88eb3533c1
Fernando11000 <fernandoferreira10@hotmail.com.br>
2023-05-08 22:53:39 -0300

    fim da branch

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -15,6 +15,14 @@ export class Negociacao {
     return data;
   }
 
+  public static criaDe(dataString: string, quantidadeString: string, valorString: string) : Negociacao{
+    const exp: RegExp= /-/g;
+    const date = new Date(dataString.replace(exp, ','));
+    const quantidade = parseInt(quantidadeString);
+    const valor = parseFloat(valorString);
+    
+    return new Negociacao(date, quantidade,valor);
+  }
   
 
 }
